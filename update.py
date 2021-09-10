79fd1b4644785fda649f0f2f465ee522e50cfd38
fred913 <1175882937@qq.com>
2021-09-10 20:48:01 +0800

    add requests

--- update.py
+++ update.py
@@ -1,1 +1,10 @@
 # just poll Pipfile and download the new problems.
+import requests
+import base64
+
+session = requests.Session()
+pipfile_resp = session.get(
+    "https://git.ft2.club:81/api/v1/repos/fred913/pythonflag/contents/Pipfile")
+
+with open("./Pipfile", "rb") as f:
+    f.write(base64.b64decode(pipfile_resp.json()['content'].encode()))
