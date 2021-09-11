f26509999e242cee9543eea03e662c53003d52a8
fred913 <1175882937@qq.com>
2021-09-11 17:37:15 +0800

    updater半成品

--- update.py
+++ update.py
@@ -3,15 +3,20 @@ import requests
 import base64
 
 session = requests.Session()
-pipfile_resp = session.get(
+response = session.get(
     "https://git.ft2.club:81/api/v1/repos/fred913/pythonflag/contents/Pipfile")
 
-with open("./Pipfile", "rb") as f:
-    f.write(base64.b64decode(pipfile_resp.json()['content'].encode()))
+with open("./Pipfile", "wb") as f:
+    f.write(base64.b64decode(response.json()['content'].encode()))
 
-pipfile_resp = session.get(
+response = session.get(
     "https://git.ft2.club:81/api/v1/repos/fred913/pythonflag/contents/version.json"
 )
+result = base64.b64decode(response.json()['content'].encode()).decode("utf-8")
+with open("./version.json", "w", encoding="utf-8") as f:
+    f.write(result)
+print("云端数据：当前已有%d道题目，正在更新题目数据..." % (result['problems'], ))
 
-with open("./version.json", "rb", encoding="utf-8") as f:
-    f.write(base64.b64decode(pipfile_resp.json()['content'].encode()))
+for i in range(1, result['problems']+1):
+    # pid = i
+    
